a11d0dc4e451c642f9c6c91c2dbfca3f4c0582ee
Christian-Fjord <fjord.christian@gmail.com>
2019-05-23 19:18:01 -0500

    Added Put, Post, and Delete endpoints

--- index.js
+++ index.js
@@ -13,6 +13,54 @@ server.use(helmet());
 
 // endpoints here
 
+// Post || Create
+
+server.post('/zoos', (req, res) => {
+const animal = req.body;
+
+  db.insert(animal)
+    .into('/zoos')
+    .then( ids => {
+      res.status(201).json(ids[0]);
+    })
+    .catch( err => {
+      res.status(500).json(err);
+    })
+})
+
+// Put || Update
+
+server.put('/zoos/:id', (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+
+  db('zoos')
+    .where('id' , '=' , id)
+    .update(changes)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch( err => {
+      res.status(500).json(err);
+    })
+})
+
+// Remove || Delete
+
+server.delete('/zoos/:id', (req, res) => {
+  const { id } = req.params;
+
+  db('zoos')
+    .where({ id })
+    .del()
+    .then( count => {
+      res.status(200).json(count)
+    })
+    .catch( err => {
+      res.status(500).json(err);
+    })
+})
+
 const port = 9393;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
